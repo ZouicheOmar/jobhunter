1544fc0b08520ec359c051f518a26b421e46fde6
ZouicheOmar <zouiche.dev.omar@gmail.com>
2026-01-03 09:01:09 +0100

    (api) city completion using zipcode

--- src/backend/src/main/java/com/jobhunter/backend/controller/CompletionController.java
+++ src/backend/src/main/java/com/jobhunter/backend/controller/CompletionController.java
@@ -6,9 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,8 +27,13 @@ public class CompletionController {
 
   @GetMapping("/city")
   public List<CityDto> ListByCityName(@RequestParam String value) {
-    // TODO test if value is string or integer (when a zipcode is sent)
-    List<City> query = cityService.findAllByNameContaining(value);
+    List<City> query;
+
+    if (value.matches("\\d+"))
+      query = cityService.findAllByZipcodeContaining(value);
+    else
+      query = cityService.findAllByNameContaining(value);
+
     return cityMapper.toAllDto(query);
   }
 }
--- src/backend/src/main/java/com/jobhunter/backend/repository/CityRepository.java
+++ src/backend/src/main/java/com/jobhunter/backend/repository/CityRepository.java
@@ -12,12 +12,8 @@ import com.jobhunter.backend.model.City;
 public interface CityRepository extends JpaRepository<City, Integer> {
   public City findByName(String name);
 
-  // Q: est ce que je dois donner %name% ou name ?
-  // name, par définition le parametre est bound entre %%
   public List<City> findAllByNameContaining(String name, Limit limit);
 
-  // Q: Est ce que ça va confondre celle définie par spring jpa avec @Query ?
-  // je crois pas
-  @Query(value = "select c from City c where c.zipcode::varchar(10) like %:zipcodeStr% limit 4", nativeQuery = true)
+  @Query(value = "select * from City where zipcode::varchar(10) like %:zipcodeStr% limit 4", nativeQuery = true)
   public List<City> findAllByZipcodeContaining(@Param("zipcodeStr") String zipcodeStr);
 }
