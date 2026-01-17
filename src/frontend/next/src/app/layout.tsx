8ca7989789ab86bb70be93bbc875c2741bad35bf
ZouicheOmar <zouiche.dev.omar@gmail.com>
2026-01-17 13:18:51 +0100

    (frontend) add highlight current path on navbar

--- src/frontend/next/src/app/layout.tsx
+++ src/frontend/next/src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import { Inter } from "next/font/google";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/Footer";
 import { Title } from "@/components/Title";
-import Link from "next/link";
 import { Nav } from "@/components/Nav";
 
-const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,25 +12,11 @@ export const metadata: Metadata = {
 	description: "next client for the jobhunter app",
 };
 
-const myFont = localFont({
-	src: "../../public/gothica1.ttf",
-	src: "../../public/Venus+Plomb.otf",
-});
-
 export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+	children, }: Readonly<{ children: React.ReactNode; }>) {
 	return (
 		<html lang="en">
-			<body
-				// className={`${roboto.className}
-				// flex flex-col md:max-w-[800px] mx-auto my-4
-				// items-center justify-items-center min-h-screen gap-2 `}
-				className={`${roboto.className}
-        md:max-w-[800px] mx-auto my-4 min-h-screen `}
-			>
+			<body className={`${roboto.className} md:max-w-[800px] mx-auto my-4 min-h-screen `} >
 				<Title />
 				<Nav />
 				{children}
--- src/frontend/next/src/components/Nav.tsx
+++ src/frontend/next/src/components/Nav.tsx
@@ -1,13 +1,33 @@
+'use client'
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const NavItem = ({ label, href }: { label: string, href: string }) =>
-	<Link className="px-2 mr-2 hover:underline" href={href}>{label}</Link>
+const NavItem = ({ label, href }: { label: string, href: string }) => {
+	const pathname = usePathname();
 
-export const Nav = () => (<>
-	<nav className="self-start shadow-sm my-4 px-2 py-1 bg-[#0b5394] rounded text-white">
-		<NavItem label="Home" href="/" />
-		<NavItem label="Candids" href="/candids" />
-		<NavItem label="Add" href="/candid/new" />
-		<NavItem label="Companies" href="/company" />
+	return (
+		<Link data-active={pathname == href}
+			className="px-2 mr-2 hover:underline
+			text-blue-200
+			data-[active=true]:underline
+			data-[active=true]:font-bold
+			data-[active=true]:text-white"
+			href={href}>
+			{label}
+		</Link>
+	)
+}
+
+export const Nav = ({ currentPath }: { currentPath?: string }) => (
+	<nav className="self-start shadow-sm my-4 p-2 bg-[#0b5394] rounded text-white flex justify-between">
+		<div>
+			<NavItem label="Home" href="/" />
+			<NavItem label="Candids" href="/candids" />
+			<NavItem label="Companies" href="/company" />
+		</div>
+		<div>
+			<NavItem label="Add" href="/candid/new" />
+		</div>
 	</nav>
-</>)
+)
