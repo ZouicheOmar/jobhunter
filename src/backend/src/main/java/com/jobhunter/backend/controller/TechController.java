aae7768af97a04aac5c6c1ace2956fd6b65b28ba
ZouicheOmar <zouiche.dev.omar@gmail.com>
2026-01-20 18:00:02 +0100

    (backend) add get techs by name route

--- src/backend/src/main/java/com/jobhunter/backend/controller/TechController.java
+++ src/backend/src/main/java/com/jobhunter/backend/controller/TechController.java
@@ -3,10 +3,14 @@ package com.jobhunter.backend.controller;
 import com.jobhunter.backend.dto.TechDto;
 import com.jobhunter.backend.mapper.TechMapper;
 import com.jobhunter.backend.service.TechService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +20,13 @@ public class TechController {
     @Autowired
     private TechService techService;
 
+    // http://localhost:8000/tech?names=name_1, name_2,name_3
     @GetMapping
-    public List<TechDto> findAll() {
-        return TechMapper.toAllDto(techService.findAll());
+    public List<TechDto> findAllByName(
+            @RequestParam String[] names) {
+        List<String> ns = Arrays.asList(names);
+        System.out.println(names);
+        System.out.print(ns);
+        return TechMapper.toAllDto(techService.findAllByNames(ns));
     }
 }
--- src/backend/src/main/java/com/jobhunter/backend/service/TechService.java
+++ src/backend/src/main/java/com/jobhunter/backend/service/TechService.java
@@ -2,7 +2,11 @@ package com.jobhunter.backend.service;
 
 import com.jobhunter.backend.model.Tech;
 import com.jobhunter.backend.repository.TechRepository;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Limit;
 import org.springframework.stereotype.Service;
@@ -25,8 +29,24 @@ public class TechService {
         return techRepository.findByName(name).orElse(save(new Tech(name)));
     }
 
-    public List<Tech> findAllByNameContaining(String websiteName) {
+    public List<Tech> findAllByNameContaining(String name) {
+        return techRepository.findAllByNameContaining(name, Limit.of(4));
+    }
+
+    public List<Tech> findAllByName(String websiteName) {
         return techRepository.findAllByNameContaining(websiteName, Limit.of(4));
+    }
+
+    public List<Tech> findAllByNames(List<String> names) {
+        List<Tech> res = new ArrayList<Tech>();
+        // TODO: raw query to get all techs from a single pg query
+
+        names.forEach(name -> {
+            Optional<Tech> tech = techRepository.findByName(name);
+            tech.ifPresent(t -> res.add(t));
+        });
+
+        return res;
     }
 
     // TODO correct this
